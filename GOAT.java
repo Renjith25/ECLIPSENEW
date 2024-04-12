c1ac4c315efa20d19061a7e128960fb105f0c08b
Renjith25 <renjithkumar.muthu@gmail.com>
2024-04-12 15:16:50 +0530

    new update

--- GOAT.java
+++ GOAT.java
@@ -7,6 +7,8 @@ public static void main(String[] args) {
 	System.out.println("who is rk");
 	System.out.println("rk is me");
 	System.out.println("oh is see");
+	System.out.println("what?");
+	
 	
 }
 
